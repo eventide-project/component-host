07041bdfcd7d1da5ced60a8bf30e13d7a7e4d629
ntl <nathanladd+github@gmail.com>
2015-11-24 09:23:35 -0600

    Refactor the reactor

--- lib/process_host.rb
+++ lib/process_host.rb
@@ -2,5 +2,6 @@ require 'connection'
 require 'telemetry/logger'
 
 require 'process_host/cooperation'
+require 'process_host/cooperation/reactor/dispatcher'
 require 'process_host/cooperation/reactor'
 require 'process_host/process_host'
