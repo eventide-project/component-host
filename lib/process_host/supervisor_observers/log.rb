0848d155df279bc584380911962888f78d4efc63
Nathan Ladd <nathan@obsidianexchange.com>
2016-12-13 10:18:12 -0600

    Include the actor class in log messages

--- lib/process_host/supervisor_observers/log.rb
+++ lib/process_host/supervisor_observers/log.rb
@@ -5,17 +5,17 @@ module ProcessHost
       include ProcessHost::Log::Dependency
 
       handle Actor::Messages::ActorStarted do |msg|
-        logger.debug "Actor started (Address: #{msg.address})"
+        logger.debug "Actor started (Address: #{msg.address}, Class: #{msg.actor.class})"
       end
 
       handle Actor::Messages::ActorStopped do |msg|
-        logger.debug "Actor stopped (Address: #{msg.address})"
+        logger.debug "Actor stopped (Address: #{msg.address}, Class: #{msg.actor.class})"
       end
 
       handle Actor::Messages::ActorCrashed do |msg|
         error = msg.error
 
-        logger.error "Error raised (ErrorClass: #{error.class.name}, Message: #{error.message.inspect})"
+        logger.error "Error raised (ErrorClass: #{error.class.name}, ActorClass: #{msg.actor.class}, Message: #{error.message.inspect})"
       end
     end
   end
